1a26366be62a0bfc807c4498323f45c11c2bb47e
ayumitamai97 <ayumitamai97@gmail.com>
2018-11-21 16:35:29 +0900

    Concatenate multiple HTML tags

--- Hello.elm
+++ Hello.elm
@@ -1,7 +1,13 @@
 module Hello exposing (..)
 
-import Html exposing (text)
+import Html exposing (div,text)
 
+hello =
+  div [] [text "hello"] ---textは何もタグで囲まれていない文字列Html a を作る
+
+world =
+  div [] [text "world"]
 
 main =
-    text "Hello"
+  div [] [ hello
+         , world]
